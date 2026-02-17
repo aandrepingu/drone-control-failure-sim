b59323e4119e19b9c6dd2cff37500f933bf355a5
aandrepingu <lungua0@gmail.com>
2026-02-17 17:37:32 -0500

    tweak ranges and add in examples for random failures

--- scripts/run_hover.py
+++ scripts/run_hover.py
@@ -8,12 +8,16 @@ import numpy as np
 import time
 
 model, data = load_model()
-failures = [ThrustLoss(start_time=3, alpha=0.01,prop=0)]
+
+# proof-of-concept of randomized failures
+failures = [
+    ThrustLoss(start_time=np.random.uniform(low=0,high=3),alpha=np.random.uniform(low=0,high=1),prop=np.random.choice(range(4)))
+    for _ in range(3)]
 
 pos_range = np.array([-0.5, 0.5])
 vel_range = np.array([0.5, 2.0])
-tilt_range = np.array([0.5, 2.0])
-yaw_range = np.array([0.5, 2.0])
+tilt_range = np.array([0.1, 0.5])
+yaw_range = np.array([0.1, 0.5])
 
 cfg = SimConfig.random(pos_range,vel_range,tilt_range, yaw_range) 
 
